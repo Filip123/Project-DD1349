a87a88f616280c7183803cb94fc98aeeecd9bd0d
filiphjertkvist <f.hjertkvist@gmail.com>
2024-04-30 15:50:29 +0200

    ADD driver dataframe

--- dataCollection.py
+++ dataCollection.py
@@ -7,12 +7,12 @@ import pandas as pd
 responseSession = urlopen('https://api.openf1.org/v1/sessions?year=2023&session_name=Race')
 dataSession = json.loads(responseSession.read().decode('utf-8'))
 
-
 keysToIncludeSession = ['session_key', 'country_key', 'year', 'date_start', 'date_end']
 
 filteredDataSession = {key: [entry[key] for entry in dataSession] for key in keysToIncludeSession}
 
 dfSession = pd.DataFrame(filteredDataSession)
+sessions2023 = dfSession['session_key'].tolist()
 print("--------Session Data---------")
 print(dfSession)
 
@@ -25,4 +25,43 @@ filteredDataCountry = {key: [entry[key] for entry in dataSession] for key in key
 dfCountry = pd.DataFrame(filteredDataCountry)
 print("--------Country Data---------")
 print(dfCountry)
+
+#Driver
+sessions2023 = dfSession['session_key'].tolist()
+allSessionData = []
+for session in sessions2023:
+    response = urlopen(f'https://api.openf1.org/v1/drivers?session_key={session}')
+    dataDriver = json.loads(response.read().decode('utf-8'))
+    allSessionData.extend(dataDriver)
+
+keysToIncludeDriver = ['driver_number','name_acronym', 'name_acronym', 'full_name']
+
+#Delete Dublicates
+dfDriver = pd.DataFrame(allSessionData)
+dfDriver = dfDriver[keysToIncludeDriver]
+dfDriver.drop_duplicates(inplace=True)
+print("--------Driver Data---------")
+print(dfDriver)
+
+
+#Driver Position
+
+# print(sessions2023)
+# print(sessions2023[0])
+# response = urlopen(f'https://api.openf1.org/v1/location?session_key={sessions2023[0]}&driver_number=1&date>2023-03-05T15:00:00&date<2023-03-05T17:00:00')
+# dataPosition = json.loads(response.read().decode('utf-8'))
+
+# keysToIncludePosition = ['driver_number', 'session_key', 'date', 'x', 'y', 'z']
+
+# filteredDataPosition = {key: [entry[key] for entry in dataPosition] for key in keysToIncludePosition}
+
+# dfPosition = pd.DataFrame(filteredDataPosition)
+# print("--------Position Data---------")
+# print(dfPosition.shape)
+
+
+
+
+
+
 
