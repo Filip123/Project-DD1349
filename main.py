6a13a2f67cfd880508b76de9e9d63471b4579278
filiphjertkvist <f.hjertkvist@gmail.com>
2024-05-03 10:35:22 +0200

    DATABASE added

--- main.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import urlopen
-import json
-
-response = urlopen('https://api.openf1.org/v1/laps?session_key=9161&driver_number=1&lap_number=8')
-data = json.loads(response.read().decode('utf-8'))
-print(data)
