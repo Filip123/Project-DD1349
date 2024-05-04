2cb18de7237e1ebc2ed878e6d91056de7cb57a66
Jonathan Lavén <92572561+JonathanBoog@users.noreply.github.com>
2024-05-04 12:34:22 +0200

    Revert "16 visual representation of car movement"

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,10 @@
 from app import app
 from flask import render_template
 
-
 #Home page
 @app.route("/")
 def index():
     return render_template("mainPage.html")
-
-def moveforwards():
-    pass
-    
-
-def movebackwards():
-    pass
-    
-   
 
 @app.route('/race-view')
 def race_view():
