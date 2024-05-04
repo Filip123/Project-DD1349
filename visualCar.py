fc1bdc676c838e589f3daa2e1e9a39fb8d3b9b5b
filiphjertkvist <f.hjertkvist@gmail.com>
2024-05-04 12:25:44 +0200

    ADD return car position

--- visualCar.py
+++ visualCar.py
@@ -1,5 +1,12 @@
 from urllib.request import urlopen
 import json
+from datetime import datetime, timedelta
+import math
+
+def getEndTime(startTime: str, timeInterval: int) -> str:
+    end_time = datetime.strptime(startTime, '%Y-%m-%dT%H:%M:%S') + timedelta(seconds=timeInterval)
+    return end_time.strftime('%Y-%m-%dT%H:%M:%S')
+
 
 def getDataApiRequest(apiRequest: str) -> any:
     response = urlopen(apiRequest)
@@ -7,18 +14,28 @@ def getDataApiRequest(apiRequest: str) -> any:
     return data
 
 def getPositionsOverTimeInterval(session, driverNumber,startTime: str, timeInterval: int) -> list:
+    endTime = getEndTime(startTime, timeInterval)
+    framesNeeded = timeInterval * 2 #To display new frame every 0.5 sec 
     positionsList = []
-    positionsData= getDataApiRequest(f'https://api.openf1.org/v1/location?session_key={session}&driver_number={driverNumber}&date>2023-03-05T16:00:00&date<2023-03-05T16:00:10')
+    positionsData= getDataApiRequest(f'https://api.openf1.org/v1/location?session_key={session}&driver_number={driverNumber}&date>{startTime}&date<{endTime}')
 
-    for position in positionsData:
+    index = 0
+    while len(positionsList) < framesNeeded:
+        position = positionsData[math.floor(index)]
         positionsList.append([position["x"], position["y"]])
-
+        index += len(positionsData) / framesNeeded
+    
     return positionsList
 
 def main():
-    position_list = getPositionsOverTimeInterval(7953, 1, "2023-03-05T16:00:00", 10)
-
+    
+    position_list = getPositionsOverTimeInterval(7953, 1, "2023-03-05T16:00:00", 5)
+    
+    print(len(position_list))
     print(position_list)
+
+    print(len(getPositionsOverTimeInterval(7953, 4, "2023-03-05T16:00:00", 5)))
+    print(getPositionsOverTimeInterval(7953, 4, "2023-03-05T16:00:00", 5))
 
 main()
 
