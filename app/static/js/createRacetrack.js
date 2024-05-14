10ce3ce01b2f9179e6b8003eea32f40ed5259b61
Jonathan Lavén <jonathan.laven123@gmail.com>
2024-05-14 19:57:35 +0200

    Add file and code to fetch coordinates for racetrack and then create divs for every coordinate

---
+++ app/static/js/createRacetrack.js
@@ -0,0 +1,29 @@
+function fetchCoordinatesForRaceTrack(startTime) {
+    console.log("Current time: " + startTime);
+    const url = '/get-racetrack-coordinates?raceStart=' + startTime;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(coordinates => {
+            const raceTrack = document.getElementById("raceTrack");
+            raceTrackIndividualOffsetX = 100;
+            raceTrackIndividualOffsetY = 0;
+
+            const scaleFactor = 24;
+
+            coordinates.forEach((coordinate) => {
+                const x = coordinate[0] / scaleFactor;
+                const y = coordinate[1] / scaleFactor;
+
+                const newDiv = document.createElement('div');
+                newDiv.className = 'coordinateDiv';
+                newDiv.style.left = `${(raceTrack.offsetWidth / 2 + x - testDriver.offsetWidth / 2) + 400}px`;
+                newDiv.style.bottom = `${(raceTrack.offsetHeight / 2 + y - testDriver.offsetHeight / 2) + 100}px`;
+
+                raceTrack.appendChild(newDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching coordinates:', error);
+        });
+}
